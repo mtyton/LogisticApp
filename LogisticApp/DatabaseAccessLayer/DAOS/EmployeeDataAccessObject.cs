e5c73302ecde76a49f6ef8c07d66e36cb36f6b7b
mtyton <mateusz.tyton99@gmail.com>
2021-07-01 21:55:14 +0200

    merged with main

--- LogisticApp/DatabaseAccessLayer/DAOS/EmployeeDataAccessObject.cs
+++ LogisticApp/DatabaseAccessLayer/DAOS/EmployeeDataAccessObject.cs
@@ -31,19 +31,12 @@ namespace LogisticApp.DatabaseAccessLayer.DAOS
             }
             return employees;
         }
-<<<<<<< Updated upstream
 
         public static List<Employee> getPaginated(int start = 0, int number = 0)
         {
             return EmployeeDataAccessObject.getAll();
         }
 
-=======
-        
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         public static Employee create(Employee employee)
         {
             using (var connection = DatabaseConnection.Instance.Connection)
