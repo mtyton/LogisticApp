b19b85cc971321fa24525fe540bed59457ed30ff
Nikodem Lewandowicz <nikolew521@student.polsl.pl>
2021-07-03 00:22:11 +0200

    one more file for order view model

---
+++ LogisticApp/ViewModel/Forms/OrderFormViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogisticApp.ViewModel.BaseClass;
+using LogisticApp.Model;
+using System.ComponentModel;
+
+namespace LogisticApp.ViewModel.Forms
+{
+    class OrderFormViewModel : BaseViewModel
+    {
+        public BaseViewModel orderFormViewModel = new OrderFormViewModel();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string jobTitle;
+
+        public string JobTitle
+        {
+            get => jobTitle;
+            set
+            {
+                jobTitle = value;
+                PropertyChanged?.Invoke(
+                    this, new PropertyChangedEventArgs(nameof(JobTitle))
+                    );
+            }
+        }
+    }
+}
