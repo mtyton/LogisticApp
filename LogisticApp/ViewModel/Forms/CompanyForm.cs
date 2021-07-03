2870163eb76375fd2629d731ae7b95c507d72363
mtyton <mateusz.tyton99@gmail.com>
2021-07-03 14:10:32 +0200

    added missing files

---
+++ LogisticApp/ViewModel/Forms/CompanyForm.cs
@@ -0,0 +1,32 @@
+﻿using LogisticApp.ViewModel.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticApp.ViewModel.Forms
+{
+    class CompanyForm : BaseViewModel
+    {
+        AddressViewModel _addressViewModel;
+
+        public AddressViewModel AddrViewModel
+        {
+            get => _addressViewModel;
+            set
+            {
+                _addressViewModel = value;
+                onPropertyChanged(nameof(AddrViewModel));
+            }
+        }
+        string _companyName;
+        string _taxNumber;
+
+        public CompanyForm()
+        {
+            AddrViewModel = new AddressViewModel();
+        }
+
+    }
+}
