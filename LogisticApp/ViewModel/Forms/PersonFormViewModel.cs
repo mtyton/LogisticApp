028d856639e2e5de76ae67456c3e85430cfa6dd9
mtyton <mateusz.tyton99@gmail.com>
2021-07-04 11:53:09 +0200

    merged with main

---
+++ LogisticApp/ViewModel/Forms/PersonFormViewModel.cs
@@ -0,0 +1,69 @@
+﻿using LogisticApp.DatabaseAccessLayer.Entity.Base;
+using LogisticApp.DatabaseAccessLayer.Entity.Client;
+using LogisticApp.Model;
+using LogisticApp.ViewModel.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticApp.ViewModel.Forms
+{
+    class PersonFormViewModel : BaseFormViewModel
+    {
+        #region {variables and properties}
+
+        AddressFormViewModel _addressViewModel;
+
+        public AddressFormViewModel AddrViewModel
+        {
+            get => _addressViewModel;
+            set
+            {
+                _addressViewModel = value;
+                onPropertyChanged(nameof(AddrViewModel));
+            }
+        }
+
+        string _taxNumber;
+
+        public string TaxNumber
+        {
+            get => _taxNumber;
+            set
+            {
+                _taxNumber = value;
+                onPropertyChanged(TaxNumber);
+            }
+        }
+
+        string _name;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                onPropertyChanged(Name);
+            }
+        }
+
+        string _surname;
+
+        public string Surname
+        {
+            get => _surname;
+            set
+            {
+                _surname = value;
+                onPropertyChanged(Surname);
+            }
+        }
+
+        #endregion
+
+
+    }
+}
